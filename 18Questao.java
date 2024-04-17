c9195a1ddd6f57774dfc5b9ce3d6edc60a94eebe
JoaoPedroSM <145557393+JoaoPedroSM@users.noreply.github.com>
2024-04-16 22:08:58 -0300

    Update 18Questao.java

--- 18Questao.java
+++ 18Questao.java
@@ -1,1 +1,25 @@
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LerCSV {
+    public static void main(String[] args) {
+        String arquivoCSV = "alunos.csv";
+
+        try (CSVReader reader = new CSVReader(new FileReader(arquivoCSV))) {
+            String[] linha;
+           
+            while ((linha = reader.readNext()) != null) {
+                for (String valor : linha) {
+                    System.out.print(valor + " ");
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo CSV.");
+            e.printStackTrace();
+        }
+    }
+}
 
